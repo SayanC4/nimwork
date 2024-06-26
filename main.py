58cf742e901ce76d73dd585528e1cc7c2b4084f9
SayanC4 <103901872+SayanC4@users.noreply.github.com>
2024-06-26 14:49:57 -0400

    Timer to main

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import nimagent as na
 from nimgame import OneHeapNimGame
+import time
 import wintables as wt
+
+start = time.time()
 
 print(wt.win_condition({1, 2, 3}))
 #print(wt.OneHeapWinTable(20, {1, 2, 3}))
@@ -17,6 +20,8 @@ def test_games(rng: range, moves: set[int], player_type: str,
   return similarities
 
 test_games(range(20, 21), {1, 2, 3}, "clever", 0.75, 0.5, True)
+
+print(f"{time.time() - start.time()} seconds")
 
 """
 TODO:
