edfae3699b283475e869e75b16225c5e5a71708b
SayanC4 <103901872+SayanC4@users.noreply.github.com>
2024-07-04 18:16:44 -0400

    Remove todo from main

--- main.py
+++ main.py
@@ -23,36 +23,3 @@ def test_games(rng: range, moves: set[int], player_type: str,
 for x in range(1, 16, 2):
   moves = [1, x, x + 1, x + 2]
   print(f"{str(moves) + ':':<12} {wt.OneHeapWinTable.win_condition(set(moves))}")
-
-"""
-TODO:
-  Make test_games() command-line based (more interface-y too)
-  Get lots of data
-  Make clever be clever(?)
-  Come up w/own questions
-    Write up coherent doc w/questions, conjectures, answers
-      Try to prove conjectures
-  Find various patterns
-    1, x, x + 1 (proven)
-      Odd:  0, 2, ... x - 1 mod 2x + 1
-      Even: 0, 2, ... x - 2 mod 2x
-    1, x, x + 2 (empirically)
-      Odd:  0, 2, ... 2x mod 2x + 2
-      Even: 0, 2, ... x - 2, x + 1, x + 3 ... 2x - 1 mod 2x + 2
-    1, x, x + 3
-      Odd:  0, 2, ... x + 1 mod 2x + 3
-      Even: ???? mod x + 4??
-    1, x, x + 4
-      Odd:  0, 2, ... 2x + 2 mod 2x + 4
-      Even: 0, 2, ... x - 2, x + 1, x + 3 ... 2x - 1 mod 2x + 4 - (except x = 2)
-    1, x, x + a?
-      a is Odd:
-        Odd:  0, 2, ... x + (a - 2) mod 2x + a
-        Even: ??? 
-      a is Even:
-        Odd:  0, 2, ... 2x + (a - 2) mod 2x + a
-        Even: 0, 2, ... x - 2, x + 1, x + 3 ... 2x - 1 mod 2x + a (mostly)  
-    1, x, x + 1, x + 2
-      Odd:  0, 2, ... x - 1 mod 2x + 2
-      Even: 0, 2, ... x - 2 mod 2x + 1
-"""
