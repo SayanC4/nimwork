9f6068442e530151a09106ad53e4efd342f344d8
SayanC4 <103901872+SayanC4@users.noreply.github.com>
2024-10-01 08:38:27 -0400

    Breachhead

--- graphing.py
+++ graphing.py
@@ -4,4 +4,7 @@ import pandas as pd
 total = pd.read_csv("./csvs/Clever {1, 2, 3}.csv")
 
 if __name__ == "__main__":
-  print(total.head())
+  for x in range(7): 
+    data = df.iloc[:104, 8*x:8*(x+1)]
+    title = data.iloc[0, 0]
+    
